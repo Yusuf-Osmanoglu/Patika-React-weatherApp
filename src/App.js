b6ef5e1d50119f6ec579c4affdc693682d047b2d
Yusuf Osmanoğlu <yusufosmanoglu2003@gmail.com>
2024-04-22 15:21:44 +0300

    Update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import WeatherApp from './Components/WeatherApp/weatherApp.jsx'; // WeatherApp bileşenini import ediyoruz
+
+function App() {
+  return (
+      <WeatherApp />
+  );
+}
+
+export default App;
+
